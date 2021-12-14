314b2f0301149cef2202456b643067ba635cd164
Andrew Lee <leeandrew@usa.com>
2021-12-14 15:10:48 -0500

    finished profile user posts rendering

--- client/src/components/ProfileDetail/ProfileDetail.jsx
+++ client/src/components/ProfileDetail/ProfileDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
 
 export default function ProfileDetail({ allPosts, currentUser}) {
 return (
@@ -9,6 +9,13 @@ return (
             <img src={currentUser.profile_pic}></img>
             <h1>{currentUser.username}</h1>
             <h1>{currentUser.bio}</h1>
+
+                {allPosts.map(post => {
+                    if (post.user_id === currentUser?.id) {
+                        return <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                    }
+                })}
+
             <section>
                 {currentUser.interests.map(interest => (
                     <h5>{interest}</h5>
