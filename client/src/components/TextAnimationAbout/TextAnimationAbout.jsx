200f2887845c0adf2a67a1b810647b8d50a8cd3f
Andrew Lee <leeandrew@usa.com>
2021-12-15 17:55:39 -0500

    new font or text animation.

---
+++ client/src/components/TextAnimationAbout/TextAnimationAbout.jsx
@@ -0,0 +1,18 @@
+import './TextAnimationAbout.css';
+export default function TextAnimationAbout() {
+
+return (
+    <div className="text-animation-box">
+        <div>
+            <p className="slide-up">The <span className="green">Daily Node</span> is a</p>
+        </div>
+        <div>
+            <p className="slide-up">centralized community</p>
+        </div>
+        <div>
+            <p className="slide-up">for decentralized ideas.</p>
+        </div>
+    </div>
+    
+);
+}
