30a7e68cdf01a1f37e589545df58890db50eeb6b
Andrew Lee <leeandrew@usa.com>
2021-12-16 02:34:50 -0500

    swapped the text animations

--- client/src/components/TextAnimation/TextAnimation.jsx
+++ client/src/components/TextAnimation/TextAnimation.jsx
@@ -1,18 +1,21 @@
 import './TextAnimation.css';
 export default function TextAnimation() {
 
+
 return (
     <div className="text-animation-box">
         <div>
-            <p className="slide-up"><span className="green">Welcome</span>,</p>
+            <p className="slide-up">The <span className="green">Daily Node</span> is a</p>
         </div>
         <div>
-            <p className="slide-up">please enjoy</p>
+            <p className="slide-up">centralized community</p>
         </div>
         <div>
-            <p className="slide-up">your stay.</p>
+            <p className="slide-up">for decentralized ideas.</p>
         </div>
     </div>
     
 );
 }
+
+
--- client/src/components/TextAnimationAbout/TextAnimationAbout.jsx
+++ client/src/components/TextAnimationAbout/TextAnimationAbout.jsx
@@ -4,13 +4,13 @@ export default function TextAnimationAbout() {
 return (
     <div className="text-animation-box">
         <div>
-            <p className="slide-up">The <span className="green">Daily Node</span> is a</p>
+            <p className="slide-up"><span className="green">Welcome</span>,</p>
         </div>
         <div>
-            <p className="slide-up">centralized community</p>
+            <p className="slide-up">please enjoy</p>
         </div>
         <div>
-            <p className="slide-up">for decentralized ideas.</p>
+            <p className="slide-up">your stay.</p>
         </div>
     </div>
     
