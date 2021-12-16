baf835c6fb08c09a126077c98753776145c241b5
Andrew Lee <leeandrew@usa.com>
2021-12-16 04:44:04 -0500

    cleaned up profiledetail

--- client/src/components/ProfileDetail/ProfileDetail.jsx
+++ client/src/components/ProfileDetail/ProfileDetail.jsx
@@ -9,22 +9,6 @@ return (
             <h1 className="username">{currentUser.username}</h1>
             <h3 className="bio">{currentUser.bio}</h3>
             <h1>Nodes: </h1>
-            <div className="profile-nodes">
-                    {/*
-                                {allPosts.map(post => (
-                                            <div>
-                                            {post.user_id === currentUser?.id ? (<Link className="profile-nodes" key={`profile-${post.title}dsss`} to={`/posts/${post.id}`}>{post.title}</Link>) : ( <></> )}
-                                            </div>
-                                        ))}
-
-
-
-                                                                allPosts.filter(post=>post.user_id === currentUser?.id).map(post=> (
-                            <Link className="profile-nodes" key={`${post.title}`} to={`/posts/${post.id}`}>{post.title}</Link>
-                            ))
-                    */}
-                    </div>
-
         </div>
 
 
